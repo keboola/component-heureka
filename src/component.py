e69bb210395c3208e6740293b2297c290c588d5d
kudj <46950237+kudj@users.noreply.github.com>
2025-02-03 13:08:52 +0100

    increase button click timeout

--- src/component.py
+++ src/component.py
@@ -82,18 +82,18 @@ class Component(ComponentBase):
         if self.cfg.country == "cz":
             page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
             page.get_by_text('Administrace e-shopu').click()
-            page.wait_for_selector('button:has-text("Přihlásit se e-mailem")', timeout=20000)
+            page.wait_for_selector('button:has-text("Přihlásit se e-mailem")', timeout=20_000)
             page.fill('#login-email', self.cfg.credentials.email)
             page.fill('#login-password', self.cfg.credentials.pswd_password)
-            page.click('button:has-text("Přihlásit se e-mailem")')
+            page.click('button:has-text("Přihlásit se e-mailem")', timeout=20_000)
 
         elif self.cfg.country == "sk":
             page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
             page.get_by_text('Administrácia e-shopu').click()
-            page.wait_for_selector('button:has-text("Prihlásiť sa e-mailom")', timeout=20000)
+            page.wait_for_selector('button:has-text("Prihlásiť sa e-mailom")', timeout=20_000)
             page.fill('#login-email', self.cfg.credentials.email)
             page.fill('#login-password', self.cfg.credentials.pswd_password)
-            page.click('button:has-text("Prihlásiť sa e-mailom")')
+            page.click('button:has-text("Prihlásiť sa e-mailom")', timeout=20_000)
 
         else:
             raise UserException("Country not supported")
